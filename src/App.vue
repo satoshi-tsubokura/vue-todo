414697c9645ed50fa1bb698c55fd25890c73c69d
satoshi-tsubokura <tsubokurajob151718@gmail.com>
2023-06-22 19:03:53 +0900

    コンテンツレイアウト作成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app>
     <HeaderOrganism />
+    <IndexPage />
   </v-app>
 </template> 
 
 <script setup>
 import HeaderOrganism from './components/organisms/HeaderOrganism.vue';
+import IndexPage from './components/pages/IndexPage.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/pages/IndexPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-main class="bg">
+    <v-container>
+      <v-row>
+        <v-col cols="0" sm="6" md="6" lg="6" xl="6">
+          <v-row>
+            <CreateForm></CreateForm>
+          </v-row>
+          <v-row>
+            <SearchForm></SearchForm>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="6" md="6" lg="6" xl="6">
+          <TodoList></TodoList>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>  
+
+<script setup>
+import CreateForm from '../organisms/CreateForm.vue';
+import SearchForm from '../organisms/SearchForm.vue';
+import TodoList from '../organisms/TodoList.vue';
+</script>
+
+<style lang="scss" scoped>
+  .bg{
+    background: url('../../assets/backgroundimg_pc.png') no-repeat top left;
+    background-size: cover;
+  }
+
+</style>
