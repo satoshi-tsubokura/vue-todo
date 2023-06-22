75a70710f0a4f65229330846b21d04a42b16ac78
satoshi-tsubokura <tsubokurajob151718@google.com>
2023-06-22 18:20:01 +0900

    ヘッダー作成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <v-app>
+    <HeaderOrganism />
   </v-app>
 </template> 
 
 <script setup>
+import HeaderOrganism from './components/organisms/HeaderOrganism.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/organisms/HeaderOrganism.vue
@@ -0,0 +1,18 @@
+<template>
+ <v-app-bar :color="'grey-darken-1'" height="80" tag="header" :flat="true" :absolute="false">
+  <v-app-bar-title class="logo" text="TodoList" tag="h1"></v-app-bar-title>
+ </v-app-bar>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  font-family: 'Roboto Slab', serif;
+  font-size: 32px;
+  font-weight: 500;
+  margin-left: 50px;
+}
+</style>
