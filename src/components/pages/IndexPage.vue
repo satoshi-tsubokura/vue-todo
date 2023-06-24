9e7b5835f48988bb3874e0a57eb0dac2d415ac28
satoshi-tsubokura <tsubokurajob151718@gmail.com>
2023-06-24 15:18:50 +0900

    レイアウトの微調整

--- src/components/pages/IndexPage.vue
+++ src/components/pages/IndexPage.vue
@@ -1,33 +1,36 @@
 <template>
   <v-main class="bg">
     <v-container>
-      <v-row>
-        <v-col cols="0" sm="6" md="6" lg="6" xl="6">
+      <v-row justify="space-around">
+        <v-col align-self="stretch" cols="0" sm="6" md="6" lg="6" xl="6">
           <v-row>
-            <CreateForm></CreateForm>
+            <v-col align-self="stretch">
+              <CreateForm></CreateForm>
+            </v-col>
           </v-row>
           <v-row>
-            <SearchForm></SearchForm>
+            <v-col align-self="stretch">
+              <SearchForm></SearchForm>
+            </v-col>
           </v-row>
         </v-col>
         <v-col cols="12" sm="6" md="6" lg="6" xl="6">
-          <TodoList></TodoList>
+          <TodoIndex></TodoIndex>
         </v-col>
       </v-row>
     </v-container>
   </v-main>
-</template>  
+</template>
 
 <script setup>
 import CreateForm from '../organisms/CreateForm.vue';
 import SearchForm from '../organisms/SearchForm.vue';
-import TodoList from '../organisms/TodoList.vue';
+import TodoIndex from '../organisms/TodoIndex.vue';
 </script>
 
 <style lang="scss" scoped>
-  .bg{
-    background: url('../../assets/backgroundimg_pc.png') no-repeat top left;
-    background-size: cover;
-  }
-
+.bg {
+  background: url('../../assets/backgroundimg_pc.png') no-repeat top left;
+  background-size: cover;
+}
 </style>
