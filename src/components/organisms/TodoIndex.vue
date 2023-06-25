a40f5966f866d85f33265c269100130d99718e4b
satoshi-tsubokura <tsubokurajob151718@gmail.com>
2023-06-25 16:37:02 +0900

    タスク一覧ボードのスタイルの修正

--- src/components/organisms/TodoIndex.vue
+++ src/components/organisms/TodoIndex.vue
@@ -1,21 +1,19 @@
 <template>
-  <ContentBoard>
+  <ContentBoard class="todo-board">
     <template #title>Todo</template>
     <template #contents>
-      <v-card>
-        <v-tabs class="todo-tabs" v-model="tab" grow hide-slider selected-class="v-tab-selected">
-          <template v-for="({ tabText }, state) in todoStateGroups" :key="state">
-            <v-tab :value="state" :border="true" rounded="0">{{ tabText }}</v-tab>
-          </template>
-        </v-tabs>
-        <v-window v-model="tab">
-          <v-window-item  v-for="({ todoList }, state) in todoStateGroups" :key="state" :value="state" reverse-transition="fade-transition" transition="fade-transition">
-            <v-list :border="true" class="todo-list">
-              <TodoItem v-for="item in todoList" :key="item.id" :todo="item" @toggle-is-done="toggleIsDone(item)"></TodoItem>
-            </v-list>
-          </v-window-item>
-        </v-window>
-      </v-card>
+      <v-tabs class="todo-tabs" v-model="tab" grow hide-slider selected-class="v-tab-selected">
+        <template v-for="({ tabText }, state) in todoStateGroups" :key="state">
+          <v-tab :value="state" :border="true" rounded="0">{{ tabText }}</v-tab>
+        </template>
+      </v-tabs>
+      <v-window v-model="tab">
+        <v-window-item  v-for="({ todoList }, state) in todoStateGroups" :key="state" :value="state" reverse-transition="fade-transition" transition="fade-transition">
+          <v-list :border="true" class="todo-list">
+            <TodoItem v-for="item in todoList" :key="item.id" :todo="item" @toggle-is-done="toggleIsDone(item)"></TodoItem>
+          </v-list>
+        </v-window-item>
+      </v-window>
     </template>
   </ContentBoard>
 </template>
@@ -69,9 +67,14 @@ function toggleIsDone(todo) {
   }
 }
 
+.todo-board {
+  height: 100%;
+}
+
 .todo-list {
   border: variables.$default-border;
   border-top: none;
   padding: 10px;
+  min-height: 100px;
 }
 </style>
